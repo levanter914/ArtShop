c4d92675bd0e107dead97c7c8ad1c6deef570c56
levanter914 <shamkashyap25@gmail.com>
2025-02-04 19:02:08 +0530

    made navbar/sorted pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Orders from "./pages/Orders";
-import Product from "./pages/Product";
-import Cart from "./pages/Cart";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import PlaceOrder from "./pages/PlaceOrder";
-import Collection from "./pages/Collection";
+import Work from "./pages/Work";
 import Navbar from "./components/Navbar";
-import Particle from "./components/Particle";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="relative">
-      {/* Particle background */}
-      {/* <Particle className="absolute top-0 left-0 w-full h-screen z-[-1]" /> */}
-
-      {/* Foreground content */}
       <div className="relative px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
+
+          <Route path="/work" element={<Work />} />
+
           <Route path="/about" element={<About />} />
+
           <Route path="/contact" element={<Contact />} />
-          <Route path="/placeorder" element={<PlaceOrder />} />
-          <Route path="/collection" element={<Collection />} />
+  
         </Routes>
+        <Footer />
       </div>
     </div>
   );
